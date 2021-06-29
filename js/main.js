ac744ede94db13bb63d66cbce9c5e0e5d225e383
prettyturtle <unifconti@gmail.com>
2021-06-29 22:50:11 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-const searchEl = document.querySelector('.search');
-// const searchInputEl = document.querySelector('.search input');
-// 4번 줄은 2번 줄과 같은 의미
-const searchInputEl = searchEl.querySelector('input');
-
-// 괄호 안의 function을 핸들러라 부른다.
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -157,5 +136,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
